25e9ae68af91b325305be46556bde7827f14b4ec
Salamun8 <52977980+Salamun8@users.noreply.github.com>
2021-02-26 06:52:28 -0500

    Added webscraping python files and other CSV data for historical stock data

---
+++ realtimedataGOOGL.py
@@ -0,0 +1,49 @@
+import re
+import json
+import csv
+from io import StringIO
+from bs4 import BeautifulSoup
+import requests
+from csv import writer
+import yfinance as yf
+from pandas_datareader import data as pdr
+import pandas as pd
+import datetime
+from datetime import date
+
+yf.pdr_override() # <== that's all it takes :-)
+
+# download dataframe using pandas_datareader
+data = pdr.get_data_yahoo("GOOGL", start=datetime.date.today(), end=datetime.date.today()+datetime.timedelta(days=1))
+
+today_date = date.today()
+
+data_iterator = pd.DataFrame(data)
+
+print(data_iterator)
+
+
+priceOpen = data['Open'].iloc[-1]
+priceHigh = data['High'].iloc[-1]
+priceLow = data['Low'].iloc[-1]
+priceClose = data['Close'].iloc[-1]
+priceAdj = data['Adj Close'].iloc[-1]
+priceVolume = data['Volume'].iloc[-1]
+df = pdr.get_data_yahoo("GOOGL", start=datetime.date.today(), end=datetime.date.today()+datetime.timedelta(days=1)).reset_index()
+df_month = df.loc[:,'Date'].iloc[-1].month
+df_day = df.loc[:,'Date'].iloc[-1].day
+df_year = df.loc[:,'Date'].iloc[-1].year
+print(priceAdj)
+print(date.today())
+print(df_month)
+print(df_day)
+print(df_year)
+
+date_format = datetime.datetime(df_year, df_month, df_day)
+date_format = date_format.strftime("%Y-%m-%d")
+print(date_format)
+
+
+with open(r'GOOGL_stocks.csv','a') as f:
+    writer=csv.writer(f)
+    writer.writerow([date_format, priceOpen, priceHigh, priceLow, priceClose, priceAdj, priceVolume])
---
+++ stockscrapingAAPL.py
@@ -0,0 +1,35 @@
+
+import re
+import json
+import csv
+from io import StringIO
+from bs4 import BeautifulSoup
+import requests
+from csv import writer
+import yfinance as yf
+
+
+stock_url = 'https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=463363200&period2=1614211200&interval=1d&events=history&includeAdjustedClose=true'
+
+response = requests.get(stock_url)
+
+file = StringIO(response.text)
+reader = csv.reader(file)
+data = list(reader)
+
+x = []
+    # print all the historical data
+for row in data:
+    print(row)
+    x = row
+
+with open(r'AAPL.csv', 'w') as stock_object:
+    writer_object = writer(stock_object) 
+    
+        # Pass the list as an argument into 
+        # the writerow()
+    for row in data: 
+        writer_object.writerow(row) 
+    
+        #Close the file object 
+    stock_object.close() 
